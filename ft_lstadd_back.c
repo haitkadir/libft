0b12719ceda9b7bc46d17b487853356586d1c0e7
Hssain Aitkadir <haitkadi@e2r8p6.1337.ma>
2022-03-30 20:20:16 +0000

    some protection added

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: haitkadi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 12:26:14 by haitkadi          #+#    #+#             */
-/*   Updated: 2021/11/15 12:26:19 by haitkadi         ###   ########.fr       */
+/*   Updated: 2022/03/30 20:15:41 by haitkadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*temp;
 
 	if (new == NULL)
-		return ;
+		exit(-1);
 	if (*lst == NULL)
 		*lst = new;
 	else
