619b0feff0b42c4b6ebf24aa9092c5fa90cd210c
Hssain Aitkadir <haitkadi@e2r8p6.1337.ma>
2022-03-30 20:19:31 +0000

    some protection added

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: haitkadi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 17:33:05 by haitkadi          #+#    #+#             */
-/*   Updated: 2021/11/14 17:33:15 by haitkadi         ###   ########.fr       */
+/*   Updated: 2022/03/30 20:11:39 by haitkadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,9 +15,11 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*temp;
 
+	if (!content)
+		exit(-1);
 	temp = (t_list *)malloc(sizeof(t_list));
 	if (!temp)
-		return (0);
+		exit(-1);
 	temp->content = content;
 	temp->next = NULL;
 	return (temp);
